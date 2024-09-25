647a11a0f109911f9cbcade8046186bb7b4cf577
jquimbayo89 <jquimbayo89@outlook.com>
2024-09-25 18:03:25 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 29,
+        skills: ['HTML', 'CSS', 'JS'],
+        salary: 25000
+    };
+
+    return member;
+}
