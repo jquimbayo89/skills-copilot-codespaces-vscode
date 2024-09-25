10a5f55de894b881672d47beef969f993cd61ed3
jquimbayo89 <jquimbayo89@outlook.com>
2024-09-25 19:03:38 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,21 @@
+// Create web server
+// Run: node comments.js
+// Open browser: http://localhost:3000
+
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+var comments = [];
+
+// Create server
+http.createServer(function (req, res) {
+    var urlObj = url.parse(req.url, true);
+    var pathname = urlObj.pathname;
+    if (pathname == '/') {
+        var filePath = path.join(__dirname, 'index.html');
+        var fileContent = fs.readFileSync(filePath);
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(fileContent);
+    } else if (pathname == '/submit') {
+        var comment = urlObj
